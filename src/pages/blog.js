0dbacbf0e3adc10ecad60932ff1b94a6cb0a9d6a
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-13 16:36:32 +0100

    dubcalendar

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,60 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const Blog = () => (
-  <Layout>
-    <h1>Latest <b>Videos, Articles & Interviews</b></h1>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+
+
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query BlogQuery {
+      allStrapiPost {
+        nodes {
+          title
+          slug
+          video
+          content {
+            data {
+              content
+            }
+          }
+          other {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+          cover {
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <h1>Latest <b>Videos, Articles & Interviews</b></h1>
+      <Link to="/">Go back to the homepage</Link>
+        {data.allStrapiPost.nodes.map((blogpost, i) => (
+         
+              <div style={{textAlign:'left'}}>
+                <GatsbyImage image={getImage(blogpost.cover.localFile.childImageSharp.gatsbyImageData)} alt={blogpost.slug} />
+                <h1><b>{blogpost.title}</b></h1>
+                <p style={{fontWeight: "var(--font-medium)"}}>{blogpost.content.data.content}</p>
+              </div>
+        ))} 
+    </Layout>
+  )
+}
+  
 
 export const Head = () => <Seo title="Blog" />
 
--- src/pages/dubcalendar.js
+++ src/pages/dubcalendar.js
@@ -1,17 +1,56 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 
-const Dubcalendar = () => (
-  <Layout>
-    <h1>Upcoming Dub & Soundsystem <b>Events in Belgium</b></h1>
-    <Link to="/">Go back to the homepage</Link>
-    
-  </Layout>
-)
+const Dubcalendar = () => {
+  const data = useStaticQuery(graphql`
+  query EventQuery {
+    allStrapiEventcalendar {
+      nodes {
+        content
+      }
+    }
+  }
+  `);
+  
+  let json = JSON.parse(data.allStrapiEventcalendar.nodes[0].content);
+  const dubEvents = json.VCALENDAR[0].VEVENT;
+  console.log(dubEvents);
+  function formatDate(dateCode){
+       let year        = dateCode.substring(0,4);
+       let month       = dateCode.substring(4,6);
+       let day         = dateCode.substring(6,8);
+       let hours        = dateCode.substring(9,11);
+       let minutes       = dateCode.substring(11,13);
+       let date        = new Date(year, month-1, day, hours, minutes);
+       let dateString = date.toDateString();
+       return dateString;
+
+     }
+
+  return(
+    <Layout>
+      <h1>Upcoming Dub & Soundsystem <b>Events in Belgium</b></h1>
+      <Link to="/">Go back to the homepage</Link>
+      <div className="eventBox">
+        {
+          dubEvents.reverse().map((dubEvent, index) => {
+              return(
+                <div className="eventCard" key={index}>
+                  <h3 className="eventTitle"> {dubEvent.SUMMARY} </h3>
+                  <div> {formatDate(dubEvent.DTSTART)} </div>
+                  <p> {dubEvent.LOCATION}</p>
+                </div>
+              )
+          })
+        }
+      </div>    
+    </Layout>
+  )
+}
 
 export const Head = () => <Seo title="Dubcalendar" />
 
