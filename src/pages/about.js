b283e1b132dc3d0dfe10af69ecca2c97e775afa5
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-17 22:25:06 +0100

    more posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ const About = () => (
       <img style={{borderRadius:'4px'}} src="../beldub-during-interview.jpeg" />
       <div>
         <h2 style={{textAlign:"left",color:'var(--color-primary)'}}> BelDub created a platform that brings you reports of everything dubbing in Belgium. It's the sequel to a documentary on Belgian soundsystem culture.</h2>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu dictum varius duis at consectetur lorem donec massa. Orci sagittis eu volutpat odio facilisis. Molestie ac feugiat sed lectus vestibulum. Sed vulputate odio ut enim blandit. Aliquet eget sit amet tellus cras. Neque volutpat ac tincidunt vitae semper quis. Condimentum id venenatis a condimentum vitae sapien. Lectus vestibulum mattis ullamcorper velit sed ullamcorper. Ac turpis egestas sed tempus urna et pharetra pharetra massa.  </p>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ipsum suspendisse ultrices gravida dictum fusce ut placerat. Arcu dictum varius duis at consectetur lorem donec massa. Orci sagittis eu volutpat odio facilisis. Molestie ac feugiat sed lectus vestibulum. Sed vulputate odio ut enim blandit. Aliquet eget sit amet tellus cras. Neque volutpat ac tincidunt vitae semper quis. Condimentum id venenatis a condimentum vitae sapien. Lectus vestibulum mattis ullamcorper velit sed ul </p>
       </div>
     </div>
 
