78ee86f107476c34cc47ff3761cd5c5a7bd9e106
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-17 19:08:09 +0100

    set html lang tag to english for SEO purposes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,7 +11,7 @@
 
 import * as React from "react"
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
   setHeadComponents([
     <link
       rel="preload"
@@ -21,5 +21,6 @@ export const onRenderBody = ({ setHeadComponents }) => {
       crossOrigin="anonymous"
       key="beldub-font"
     />,
-  ])
+  ]);
+  setHtmlAttributes({ lang: "en" })
 }
