a4d8f2acbaeae2fe65ccfbb4357507336fec50a7
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-24 05:24:35 +0100

    react-leaflet bug fix attempt

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,19 @@ exports.createPages = async ({ graphql,  actions }) => {
 
   ))
 }
+exports.onCreateWebpackConfig = ({ stage, rules, loaders, actions }) => {
+  switch (stage) {
+    case 'build-html':
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /react-leaflet/,
+              use: [loaders.null()]
+            }
+          ]
+        }
+      });
+      break;
+  }
+};
