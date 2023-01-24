688e1ade330ad6117c04727539e4b41325cfc327
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-24 05:27:17 +0100

    revert

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,19 +41,4 @@ exports.createPages = async ({ graphql,  actions }) => {
 
   ))
 }
-exports.onCreateWebpackConfig = ({ stage, rules, loaders, actions }) => {
-  switch (stage) {
-    case 'build-html':
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /react-leaflet/,
-              use: [loaders.null()]
-            }
-          ]
-        }
-      });
-      break;
-  }
-};
+
