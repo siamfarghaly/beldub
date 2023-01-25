8a5394ffc47c938a46061c6b5df9cba494fd06cd
siamfarghaly <siam_farghaly@hotmail.be>
2023-01-25 04:27:00 +0100

    social sharing debug

--- src/templates/sound-page.js
+++ src/templates/sound-page.js
@@ -116,7 +116,7 @@ export const query = graphql`
 
 export const Head = ({data}) => (
   <Seo title={data.strapiSoundsystem.name}>
-    <meta name="og:image" content={data.strapiSoundsystem.img.formats.thumbnail.url} />
+    <meta property="og:image" content={data.strapiSoundsystem.img.formats.thumbnail.url} />
     <meta name="image" content={data.strapiSoundsystem.img.formats.thumbnail.url} />
   </Seo>
   )
