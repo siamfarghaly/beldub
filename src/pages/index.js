65e335529a6a29d6e37dfe7fd0c8cbe3174a8dcb
siamfarghaly <siam_farghaly@hotmail.be>
2023-04-14 16:40:11 +0200

    hero video

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import "../components/index.module.css"
+import HeroVideo from "../assets/hero-video.mp4"
 
 
 const IndexPage = props => {
@@ -11,13 +12,18 @@ const IndexPage = props => {
   const allPosts = data.allStrapiPost.nodes
   return (
     <Layout>
-      <div className="hero" style={{ zIndex: '-100', position: 'absolute', top: '250px', overflow: 'hidden', left: '0', backgroundPosition: '90%', height: '60vh', width: '100vw', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundImage: 'url("../beldubcover.jpg")' }}>
+      <div className="hero video-container">
+      <video className="hero-video" loop autoPlay muted poster="../beldubcover.jpg">
+        <source src={HeroVideo} type="video/mp4" />
+      </video>
       </div>
-      <div className="hero" style={{ zIndex: '-105', position: 'absolute', top: '100px', overflow: 'hidden', left: '0', backgroundPosition: '0%', height: '60vh', width: '100vw', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'black' }}>
+      {/* <div className="hero" style={{ zIndex: '-100', position: 'absolute', top: '250px', overflow: 'hidden', left: '0', backgroundPosition: '90%', height: '60vh', width: '100vw', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundImage: 'url("../beldubcover.jpg")' }}>
+      </div> */}
+      <div className="hero" style={{ zIndex: '-105', position: 'absolute', top: '100px', overflow: 'hidden', left: '0', backgroundPosition: '0%', height: '60vh', width: '100vw', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundColor: 'var(--color-primary)' }}>
       </div>
       <div style={{ height: '68vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginBottom: '12vh' }}>
-        <h1 style={{ fontSize: '2.5rem', textAlign: 'center', color: 'var(--color-white)' }}>
-          The platform that brings you reports of everything <b>dubbing in Belgium.</b>
+        <h1 style={{ fontSize: '2.5rem', textAlign: 'center', color: 'var(--color-white)', textShadow:'2px 4px 5px var(--color-black)' }}>
+          The platform that brings you reports of everything <b style={{color:'var(--color-primary)'}}>dubbing in Belgium.</b>
         </h1>
         <a href="https://vimeo.com/201431711" className="actionButton" style={{ padding: '1rem', textAlign: 'center', textDecoration: 'none', cursor: 'pointer', boxShadow: "4px 6px 5px var(--color-blue)", margin: '0 auto', width: '300px', color: 'var(--color-primary)', border: '2px solid var(--color-primary)', borderRadius: '4px', backgroundColor: '#161615', height: '65px' }}>
           <h3 style={{ marginTop: '0', marginBottom: 'var(--space-3)' }}>WATCH DOCUMENTARY</h3>
