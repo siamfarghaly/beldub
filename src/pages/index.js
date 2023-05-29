371919b822edc5f6c9b57d5556ab83092312608e
siamfarghaly <siam_farghaly@hotmail.be>
2023-05-30 00:34:07 +0200

    fix broken links dub reports

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ const IndexPage = props => {
       <div className="latestPosts" style={{ display: "grid", gridTemplateColumns: "repeat( auto-fit, minmax(300px, 1fr) )", gridGap: "30px", marginTop: "30px" }}>
         {allPosts.map((blogpost, i) => (
           <div key={i} style={{ maxWidth: '600px', textAlign: 'left' }}>
-            <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={"blog/" + blogpost.slug}>
+            <Link style={{ color: 'inherit', textDecoration: 'inherit' }} to={"dub-reports/" + blogpost.slug}>
               <GatsbyImage style={{ borderRadius: '4px' }} image={getImage(blogpost.cover.localFile.childImageSharp.gatsbyImageData)} alt={blogpost.slug} />
               <p style={{ fontSize: 'var(--font-sm)', marginTop: 'var(--space-1)', marginBottom: '0' }}>{blogpost.publishedAt}</p>
               <h2 style={{ lineHeight: 'var(--line-height-dense)', color: 'var(--color-primary)', marginTop: 'var(--space-1)', marginBottom: 'var(--space-2)', textAlign: 'left' }}><b>{blogpost.title}</b></h2>
@@ -48,7 +48,7 @@ const IndexPage = props => {
         ))}
       </div>
       <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', marginBottom: 'var(--space-4)' }}>
-        <a href="/blog" className="actionButton" style={{ padding: '1rem', textAlign: 'center', textDecoration: 'none', cursor: 'pointer', boxShadow: "4px 6px 5px var(--color-blue)", margin: '0 auto', width: '300px', color: 'var(--color-primary)', border: '2px solid var(--color-primary)', borderRadius: '4px', backgroundColor: '#161615', height: '65px' }}>
+        <a href="/dub-reports" className="actionButton" style={{ padding: '1rem', textAlign: 'center', textDecoration: 'none', cursor: 'pointer', boxShadow: "4px 6px 5px var(--color-blue)", margin: '0 auto', width: '300px', color: 'var(--color-primary)', border: '2px solid var(--color-primary)', borderRadius: '4px', backgroundColor: '#161615', height: '65px' }}>
           <h3 style={{ marginTop: '0', marginBottom: 'var(--space-3)' }}>VIEW ALL</h3>
         </a>
       </div>
